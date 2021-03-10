aaa31b1f25a35983f32d36a57630980c389bbe7a
DmitryTsikalovsky <dimitry.tsikalovsky@gmail.com>
2021-03-10 18:13:59 +0300

    New addUser

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ db = client["charbotbd"]
 colLessons = db["lessons"]
 colUsers = db["users"]
 
-# Поток:
-# Получить институт
-# Получить курс
-# Обратиться к функции 1 и получить от неё имя предмета
 def searchLessonsName(instituteName, semestrNumber, colLessons):
     lessonsList = colLessons.find({"institutename": instituteName, "semestr": semestrNumber})
     message = "Доступные предметы: \n"
@@ -22,13 +18,10 @@ def searchLessonsName(instituteName, semestrNumber, colLessons):
         array.append(item["lessonname"])
         message = message + str(i+1) + ")" + item["lessonname"] + "\n"
         i += 1
-    # Введите номер предмета который вам нужен
-    # Получить число и проверить на i
-    #Отправляем сформированное сообщение, получаем цифру, выбранную пользователем, и возвращаем имя предмата
-    return array[i-1] #Поменять i на номер (на один меньше) который присылает пользователь
-    # Возвращаем введёный номер предмета
 
-# message = searchLessonsName("ИИКС", 1, colLessons)
+    return array[i-1]
+
+# lessonname = searchLessonsName("ИИКС", 1, colLessons)
 
 def searchHelp(instituteName, semestrNumber, lessonName, colUsers):
     usersList = colUsers.find({"status": True,"lessons": { '$elemMatch': {"institutename": instituteName, "semestr": semestrNumber, "lessonname":lessonName}}})
@@ -37,22 +30,19 @@ def searchHelp(instituteName, semestrNumber, lessonName, colUsers):
         message = message + "@" + item["vkid"] + "(" + item["username"] + ")\n"
 
     return message #@1231231(Дмитирй)\n
-# Отправили пользователю
-#searchHelp("ИИКС", 1, "Матанализ", colUsers)
 
-# Поток:
-# Получтить вкайди
-# Узнать институт
-# Узнать семестр
-# Обратиться к функции номер 1 - получить имя предмета
-# Обратиться к функции номер 2 - добавить предмет к пользоватею
+# messages = searchHelp("ИИКС", 1, "Матанализ", colUsers)
 
-def addUserToLesson(vkId, instituteName, semestrNumber,lessonName, colUsers):
-    check = colUsers.find_one({"vkid": vkId, "lessons": { '$elemMatch': {"institutename": instituteName, "semestr": semestrNumber, "lessonname":lessonName}}})
-    if check == None:
-        user = colUsers.update_one({"vkid": vkId}, { '$push': { "lessons": { "institutename": instituteName, "semestr": semestrNumber, "lessonname": lessonName } } })
+def addUserToLesson(vkId, instituteName, semestrNumber,lessonName, userName, colUsers):
+    user = colUsers.find_one({"vkid": vkId})
+    if user:
+        check = colUsers.find_one({"vkid": vkId, "lessons": { '$elemMatch': {"institutename": instituteName, "semestr": semestrNumber, "lessonname":lessonName}}})
+        if check == None:
+            user = colUsers.update_one({"vkid": vkId}, { '$push': { "lessons": { "institutename": instituteName, "semestr": semestrNumber, "lessonname": lessonName } } })
+    else:
+        colUsers.insert_one({"vkid": vkId, "username": userName, "status": True, "lessoms": [{ "institutename": instituteName, "semestr": semestrNumber, "lessonname": lessonName}]})
 
-# addUserToLesson("03092003", "ИИКС", 1, "Маанализ", colUsers )
+addUserToLesson("123456", "ИИКС", 1, "Дискретная математика","Цикаловский Дмитрий", colUsers )
 
 
 # vk_session = vk_api.VkApi(token = tok)
