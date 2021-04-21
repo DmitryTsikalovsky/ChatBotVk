482b7295524ad10963356e725a05bccea8cea02b
Alice Bloshenkina <48186115+AliceBloshenkina@users.noreply.github.com>
2021-04-21 20:46:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import vk_api, json
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 main_token = "ee861bd7c946a561144d9a2899379557354701e61c9f44d19ff828e17a5cfc5166822c13addb40fafeefc"  # моя группа
-# main_token = "8c20fd59534bd074cc126ee189b7d533a06fb0d92c70ba6f9ad629d0e7e956ec2ce599cd6b12b11e377c8" # чат бот в группе
+#main_token = "8c20fd59534bd074cc126ee189b7d533a06fb0d92c70ba6f9ad629d0e7e956ec2ce599cd6b12b11e377c8" # чат бот в группе
 
 # Подключение к вк
 vk_session = vk_api.VkApi(token=main_token)
@@ -37,18 +37,23 @@ def searchLessonsName(instituteName, semestrNumber, colLessons, event, start_key
         if event.type == VkEventType.MESSAGE_NEW:
             if event.to_me:
                 request = event.text.lower()
-                i = int(request)
-                lessonName = array[i - 1]
-                if temp == 1:
-                    message = searchHelp(instituteName, semestrNumber, lessonName, colUsers)
-                    write_msg(event.user_id, f"Ваши данные: {message}", start_key)
-                    return array[i - 1]
-                if temp == 2:
-                    addUserToLesson(event.user_id, instituteName, semestrNumber, lessonName, userName, colUsers)
-                    write_msg(event.user_id,
-                              f"Данные успешно обновлены {event.user_id}, {instituteName}, {semestrNumber}, {lessonName}",
-                              start_key)
-                    return array[i - 1]
+                if request.isdigit() and int(request) > i + 1:
+                    write_msg_text(event.user_id, "Некорректный ввод данных. Попробуйте еще раз.")
+                elif request.isdigit():
+                    i = int(request)
+                    lessonName = array[i - 1]
+                    if temp == 1:
+                        message = searchHelp(instituteName, semestrNumber, lessonName, colUsers)
+                        write_msg(event.user_id, f"Ваши данные: {message}", start_key)
+                        return array[i - 1]
+                    if temp == 2:
+                        addUserToLesson(event.user_id, instituteName, semestrNumber, lessonName, userName, colUsers)
+                        write_msg(event.user_id,
+                                "Данные успешно обновлены",
+                                start_key)
+                        return array[i - 1]
+                else:
+                    write_msg_text(event.user_id, "Некорректный ввод данных. Попробуйте еще раз.")
 
 
 def searchHelp(instituteName, semestrNumber, lessonName, colUsers):
@@ -62,16 +67,17 @@ def searchHelp(instituteName, semestrNumber, lessonName, colUsers):
 
 
 def addUserToLesson(vkId, instituteName, semestrNumber, lessonName, userName, colUsers):
+    lesson = colLessons.find_one({"institutename": instituteName, "semestr": semestrNumber, "lessonname": lessonName})
     user = colUsers.find_one({"vkid": vkId})
     if user:
         check = colUsers.find_one({"vkid": vkId, "lessons": {
             '$elemMatch': {"institutename": instituteName, "semestr": semestrNumber, "lessonname": lessonName}}})
         if check == None:
             user = colUsers.update_one({"vkid": vkId}, {'$push': {
-                "lessons": {"institutename": instituteName, "semestr": semestrNumber, "lessonname": lessonName}}})
+                "lessons": {"institutename": lesson["institutename"], "semestr": semestrNumber, "lessonname": lessonName}}})
     else:
         colUsers.insert_one({"vkid": vkId, "username": userName, "status": True, "lessons": [
-            {"institutename": instituteName, "semestr": semestrNumber, "lessonname": lessonName}]})
+            {"institutename": lesson["institutename"], "semestr": semestrNumber, "lessonname": lessonName}]})
 
 
 def get_keyboard(buts):
@@ -129,7 +135,7 @@ def getUserLessons(vkId, colUsers):
     if user:
         message = ""
         for lesson in user["lessons"]:
-            message = message + str(i) + ")" + str(lesson["semestr"]) + ' семестр ' + lesson["institutename"] + ' ' + \
+            message = message + str(i) + ")" + str(lesson["semestr"]) + ' семестр ' + lesson["institutename"][0] + ' ' + \
                       lesson["lessonname"] + "\n"  # 1) 1 семестр ИИКС Матанализ
             i += 1
     else:
@@ -143,6 +149,13 @@ def getStatus(vkId, colUsers):
         return 1
     else:
         return 0
+
+def check_institute(instituteName):
+    names = ['ияфит', 'лаплаз', 'ифиб', 'интэл', 'иикс', 'ифтис', 'ифтэб', 'имо', 'фбиукс']
+    for name in names:
+        if instituteName in name:
+            return 1
+    return 0
 
 
 def changeStatus(vkId, colUsers):
@@ -160,6 +173,8 @@ def write_msg_text(id, text):
 def write_msg(id, text, key):
     vk_session.method('messages.send', {'user_id': id, 'message': text, 'random_id': 0, 'keyboard': key})
 
+def message_violation(text):
+    vk_session.method('messages.send', {'user_id': '361771852', 'message': f'СООБЩЕНИЕ О НАРУШЕНИИ: \n {text}', 'random_id': 0})
 
 def main():
     users = []
@@ -180,7 +195,8 @@ def main():
     choice_zareg_key = get_keyboard(
         [
             [('Ищу помощь', 'синий')],
-            [('Мой профиль', 'белый')]
+            [('Мой профиль', 'белый')],
+            [('Сообщить о нарушении', 'синий')]
         ]
     )
 
@@ -213,6 +229,7 @@ def main():
     add_lesson = 0
     aktiv_check = 0
     temp_tst = 0
+    violation = 0
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
@@ -221,21 +238,36 @@ def main():
                 request = event.text.lower()
                 name = get_name(event.user_id)
                 info = vk_session.method('users.get', {'user_ids': event.user_id})
+                check = 0
 
                 if step == 1 and status == 0:
                     write_msg(event.user_id, "Выбирайте: ", choice_key)
+                    check = 1
 
                 if step == 1 and status == 1:
                     write_msg(event.user_id, "Выбирайте: ", choice_zareg_key)
+                    check = 1
 
                 if step == 2 and status == 1 and request == 'мой профиль':
                     write_msg(event.user_id, "Выбирайте: ", profil_key)
+                    check = 1
+
+                if violation == 1:
+                    message_violation(request)
+                    violation = 0
+                    check = 1
+
+                if step == 2 and status == 1 and request == 'сообщить о нарушении':
+                    write_msg_text(event.user_id, "Введите сообщение, которое будет передано администраторам: ")
+                    violation = 1
+                    check = 1
 
                 if (step == 3 and status == 1) or (aktiv_check == 1):
                     if request == 'статус' or aktiv_check == 1:
                         if aktiv_check == 0:
                             write_msg(event.user_id, "Выбирайте: ", status_key)
                             aktiv_check = 1
+                            check = 1
                         if request == 'показать статус':
                             aktiv = getStatus(event.user_id, colUsers)
                             if aktiv == 1:
@@ -245,21 +277,25 @@ def main():
                             write_msg(event.user_id, f"Ваш статус: '{aktiv}'", start_key)
                             step = 0
                             aktiv_check = 0
+                            check = 1
                         if request == 'изменить статус':
                             changeStatus(event.user_id, colUsers)
                             write_msg(event.user_id, "Ваш статус изменен", start_key)
                             step = 0
                             aktiv_check = 0
+                            check = 1
 
                     if request == 'показать предметы':
                         text = getUserLessons(event.user_id, colUsers)
                         write_msg(event.user_id, f"Ваши данные:\n {text}", start_key)
                         step = 0
+                        check = 1
 
                     if request == 'добавить предмет':
                         add_lesson = 1
                         what_want = 'хочу помочь'
                         write_msg(event.user_id, "Выберите институт", institute_key)
+                        check = 1
 
                     if request == 'удалить предмет' or item == 1:
                         if item == 0:
@@ -272,35 +308,45 @@ def main():
                             write_msg(event.user_id, "Предмет успешно удален", start_key)
                             step = 0
                             item = 0
+                        check = 1
 
                 if (step == 2 and status == 0) or (step == 2 and status == 1 and request == 'ищу помощь'):
                     what_want = request
                     write_msg(event.user_id, "Выберите институт", institute_key)
                     temp_tst = 1
+                    check = 1
 
                 if (step == 3 and status == 0) or (add_lesson == 2) or (step == 3 and status == 1 and temp_tst == 1):
                     instituteName = request
-                    instituteName = instituteName.upper()
-                    write_msg_text(event.user_id, "Введите семестр")
-                    if temp_tst == 1:
-                        temp_tst += 1
+                    check = check_institute(instituteName)
+                    if check == 1:
+                        instituteName = instituteName.upper()
+                        write_msg_text(event.user_id, "Введите семестр")
+                        if temp_tst == 1:
+                            temp_tst += 1
+                    
+                if (step == 4 and status == 0) or (add_lesson == 3) or (step == 4 and status == 1 and temp_tst == 2 and add_lesson == 0):
+                    if request.isdigit():
+                        semestrNumber = request
+                        semestrNumber = int(semestrNumber)
+                        if semestrNumber > 0 and semestrNumber < 12:
+                            if what_want == 'ищу помощь':
+                                lessonsName = searchLessonsName(instituteName, semestrNumber, colLessons, event, start_key,
+                                                                1, colUsers, name)
+                            if what_want == 'хочу помочь':
+                                lessonsName = searchLessonsName(instituteName, semestrNumber, colLessons, event, start_key,
+                                                                2, colUsers, name)
+                        step = 0
+                        add_lesson = 0
+                        check = 1
 
-                if (step == 4 and status == 0) or (add_lesson == 3) or (step == 4 and status == 1 and temp_tst == 2):
-                    semestrNumber = request
-                    semestrNumber = int(semestrNumber)
-                    if semestrNumber > 0 and semestrNumber < 12:
-                        if what_want == 'ищу помощь':
-                            lessonsName = searchLessonsName(instituteName, semestrNumber, colLessons, event, start_key,
-                                                            1, colUsers, name)
-                        if what_want == 'хочу помочь':
-                            lessonsName = searchLessonsName(instituteName, semestrNumber, colLessons, event, start_key,
-                                                            2, colUsers, name)
-                    step = 0
-                    add_lesson = 0
-
-                step += 1
-                if add_lesson != 0:
-                    add_lesson += 1
+                if check == 1:
+                    step += 1
+                    if add_lesson != 0:
+                        add_lesson += 1
+                else:
+                    write_msg_text(event.user_id, "Некорректный ввод данных. Попробуйте еще раз.")
+                
 
 
 while True:
