f694fca33bc24c51f588b9aa4e9974c78975bc9c
DmitryTsikalovsky <dimitry.tsikalovsky@gmail.com>
2021-04-29 23:40:53 +0300

    Links functions

--- main.py
+++ main.py
@@ -1,49 +1,51 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-from config import tok, group_id
-from pymongo import MongoClient
-import pymongo
 
-client = MongoClient("mongodb+srv://al1ewnare:03092003@chatbotvk.qda4d.mongodb.net/charbotbd?retryWrites=true&w=majority")
+from pymongo import MongoClient
+import time
+import pymongo
+import requests
+import vk_api, json
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+# main_token = "8c20fd59534bd074cc126ee189b7d533a06fb0d92c70ba6f9ad629d0e7e956ec2ce599cd6b12b11e377c8" # чат бот в группе
+
+# # Подключение к вк
+# vk_session = vk_api.VkApi(token=main_token)
+# session_api = vk_session.get_api()
+# longpoll = VkLongPoll(vk_session)
+
+# Подключение к базе данных
+client = MongoClient(
+    "mongodb+srv://al1ewnare:03092003@chatbotvk.qda4d.mongodb.net/charbotbd?retryWrites=true&w=majority")
 db = client["charbotbd"]
 colLessons = db["lessons"]
 colUsers = db["users"]
+colLink = db["literature"]
 
-def searchLessonsName(instituteName, semestrNumber, colLessons):
-    lessonsList = colLessons.find({"institutename": instituteName, "semestr": semestrNumber})
-    message = "Доступные предметы: \n"
-    array = []
-    i = 0
-    for item in lessonsList:
-        array.append(item["lessonname"])
-        message = message + str(i+1) + ")" + item["lessonname"] + "\n"
-        i += 1
 
-    return array[i-1]
 
-# lessonname = searchLessonsName("ИИКС", 1, colLessons)
-
-def searchHelp(instituteName, semestrNumber, lessonName, colUsers):
-    usersList = colUsers.find({"status": True,"lessons": { '$elemMatch': {"institutename": instituteName, "semestr": semestrNumber, "lessonname":lessonName}}})
+def searchLink(instituteName, semestrNumber, lessonName, colLink):
+    LinkList = colLink.find({"institutename": instituteName, "semestr": semestrNumber, "lessonname": lessonName})
     message = ""
-    for item in usersList:
-        message = message + "@" + item["vkid"] + "(" + item["username"] + ")\n"
-
-    return message #@1231231(Дмитирй)\n
-
-# messages = searchHelp("ИИКС", 1, "Матанализ", colUsers)
-
-def addUserToLesson(vkId, instituteName, semestrNumber,lessonName, userName, colUsers):
-    user = colUsers.find_one({"vkid": vkId})
-    if user:
-        check = colUsers.find_one({"vkid": vkId, "lessons": { '$elemMatch': {"institutename": instituteName, "semestr": semestrNumber, "lessonname":lessonName}}})
-        if check == None:
-            user = colUsers.update_one({"vkid": vkId}, { '$push': { "lessons": { "institutename": instituteName, "semestr": semestrNumber, "lessonname": lessonName } } })
-    else:
-        colUsers.insert_one({"vkid": vkId, "username": userName, "status": True, "lessoms": [{ "institutename": instituteName, "semestr": semestrNumber, "lessonname": lessonName}]})
-
-addUserToLesson("123456", "ИИКС", 1, "Дискретная математика","Цикаловский Дмитрий", colUsers )
+    i = 1
+    for item in LinkList:
+        message = message + str(i) + ")" +item["title"] + "\n"
+        if len(item["autors"]) != 0:
+            autormessage = "Авторы: "
+            for autor in item["autors"]:
+                autormessage = autormessage + autor + " "
+            message = message + autormessage + "\n"
+        message = message + item["href"] + "\n"
+        i += 1
+    return message
 
 
-# vk_session = vk_api.VkApi(token = tok)
-# longpoll = VkLongPoll(vk_session, group_id)
+result = searchLink(["ИИКС"], 1, "Математический анализ", colLink)
+a = result
+
+def addLink(instituteName, semestrNumber, lessonName, title, autors , href, colLnik):
+        colLnik.insert_one({"institutename": instituteName, "semestr": semestrNumber, "lessonname": lessonName, "title": title, "autors": autors, "href": href})
+addLink(["ИИКС"], 1,"Математический анализ","Кр 2 Окороков", [], "lalla", colLink)
+
+
